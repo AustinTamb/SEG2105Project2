70ae6482f8e20632f29ef0ad343b293b71f77a28
Raymond Chang <rchan086@uottawa.ca>
2017-11-28 17:03:34 -0500

    Made the New Task button work

---
+++ app/src/main/java/com/uottawa/choremanager/newTaskActivity.java
@@ -0,0 +1,21 @@
+package com.uottawa.choremanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.view.View;
+
+public class newTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_task);
+
+    }
+}
--- app/src/main/java/com/uottawa/choremanager/tasksFragment.java
+++ app/src/main/java/com/uottawa/choremanager/tasksFragment.java
@@ -1,11 +1,17 @@
 package com.uottawa.choremanager;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import static java.sql.DriverManager.println;
 
 /**
  * Created by Raymo on 2017-11-24.
@@ -14,14 +20,31 @@ import android.view.ViewGroup;
 public class tasksFragment extends Fragment {
     //taken from tutorial https://www.youtube.com/watch?v=bNpWGI_hGGg
 
+    private ImageButton btnNewTask;
+
     private static final String TAG = "tasksFragment";
+
+
+    //This nested class is used to control what happens when btnNewTask is clicked
+    public class NewTaskOnClickListener implements View.OnClickListener{
+        public void onClick(View v) {
+            Intent newTaskIntent = new Intent(getActivity().getApplicationContext(), newTaskActivity.class);
+            startActivity(newTaskIntent);
+        }
+    }
 
     @Nullable
     @Override
     public View onCreateView (LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tasks, container, false);
+        btnNewTask = (ImageButton) view.findViewById(R.id.btnNewTask);
+        btnNewTask.setOnClickListener(new NewTaskOnClickListener());
+
 
         return view;
     }
+
+
+
     //End of citation
 }
