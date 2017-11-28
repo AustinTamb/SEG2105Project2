e42996e53e00f95525b4108af98bcb4e4825d3de
Raymond Chang <rchan086@uottawa.ca>
2017-11-27 21:27:32 -0500

    Added Profile, SubTask, Task 	Raymond and Austin

---
+++ app/src/main/java/com/uottawa/choremanager/Profile.java
@@ -0,0 +1,66 @@
+package com.uottawa.choremanager;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Calendar;
+
+public class Profile{
+	private boolean isParent;
+	private String name;
+	private ArrayList<Task> assignedTasks;
+
+	public Task createTask(String name, Date startDate, Boolean recur, int recurRate, Date endDate, Profile owner){
+		
+
+		if(owner!=null){
+			//set task owner to owner
+			//add task to owners assignedTasks
+		}
+
+		return null;
+	}
+
+	public Profile(String name, boolean isParent/*Add image*/){
+		this.isParent = isParent;
+		this.name = name;
+	}
+
+	public void takeTask(Task toTake){
+		if(!toTake.getDone()){
+			Profile oldOwner = toTake.getOwner();
+			if(oldOwner != null)
+				oldOwner.removeTask(toTake);
+			
+			assignedTasks.add(toTake);
+			toTake.setOwner(this);
+		}
+
+	}
+
+	public void removeTask(Task toTake){
+		if(assignedTasks.contains(toTake)){
+			assignedTasks.remove(toTake);
+		}
+	}
+
+	public void setName(String newName){
+		this.name = newName;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public void setParent(Boolean isParent){
+		this.isParent = isParent;
+	}
+
+	public Boolean isParent(){
+		return isParent;
+	}
+
+	public int getNumberOfCurrentTasks(){
+		return assignedTasks.size();
+	}
+
+}
