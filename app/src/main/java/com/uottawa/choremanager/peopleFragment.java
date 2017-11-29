9bdecba1b21ea86a175f93f64a91e28f91a32a0e
Raymond Chang <rchan086@uottawa.ca>
2017-11-28 22:29:19 -0500

    Added newProfileAcitivity

--- app/src/main/java/com/uottawa/choremanager/peopleFragment.java
+++ app/src/main/java/com/uottawa/choremanager/peopleFragment.java
@@ -1,11 +1,13 @@
 package com.uottawa.choremanager;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 /**
  * Created by Raymo on 2017-11-24.
@@ -14,13 +16,25 @@ import android.view.ViewGroup;
 public class peopleFragment extends Fragment {
     //taken from https://www.youtube.com/watch?v=bNpWGI_hGGg
 
+    private ImageButton btnNewProfile;
+
     private static final String TAG = "peopleFragment";
+
+    //This nested class is used to control what happens when btnNewTask is clicked
+    public class NewProfileOnClickListener implements View.OnClickListener{
+        public void onClick(View v) {
+            Intent newTaskIntent = new Intent(getActivity().getApplicationContext(), newTaskActivity.class);
+            startActivity(newTaskIntent);
+        }
+    }
+
 
     @Nullable
     @Override
     public View onCreateView (LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.people, container, false);
-
+        btnNewProfile = (ImageButton) view.findViewById(R.id.imgNewPerson);
+        btnNewProfile.setOnClickListener(new NewProfileOnClickListener());
         return view;
     }
     //End of citation
