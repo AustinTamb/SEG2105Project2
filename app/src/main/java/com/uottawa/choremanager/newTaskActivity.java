72c5917b1955b6b3230c8fd563d47b49f17adb56
Raymond Chang <rchan086@uottawa.ca>
2017-11-30 10:14:15 -0500

    Trying to make it so that tasks can be added, only missing spinner now

--- app/src/main/java/com/uottawa/choremanager/newTaskActivity.java
+++ app/src/main/java/com/uottawa/choremanager/newTaskActivity.java
@@ -1,8 +1,10 @@
 package com.uottawa.choremanager;
 
 import android.app.ListActivity;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
 import android.util.Log;
 import android.view.View;
 
@@ -15,6 +17,7 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,37 +25,97 @@ import java.util.List;
 public class newTaskActivity extends AppCompatActivity {
     private ArrayList<SubTask> subTasks;
     private ArrayList<String> names;
-    private ArrayAdapter<String> aA;
+    private newTaskMaterialsAdapter materialsTasksAdapter;
+    private DataBase dB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_task);
         names = new ArrayList<String>();
+        subTasks = new ArrayList<SubTask>();
         final Spinner profiles = findViewById(R.id.spnProfiles);
         //Get array of Profiles, loop through and getNames->spinnerOption
 
-        final Button button = findViewById(R.id.btnAddTask);
-        button.setOnClickListener(new View.OnClickListener() {
+        ListView test =(ListView) findViewById(R.id.listViewMaterials);
+        String testStr = test.toString();
+
+
+        ListView materialsListView = (ListView) findViewById(R.id.listViewMaterials);
+        System.out.println("-------------------------------------------------> PRINTING ID" + testStr);
+
+
+        materialsTasksAdapter = new newTaskMaterialsAdapter(getApplicationContext(),subTasks);
+        materialsListView.setAdapter(materialsTasksAdapter);
+
+
+
+        final Button addTaskButton = findViewById(R.id.btnAddTask);
+        addTaskButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                String startDate = ((TextView)findViewById(R.id.txtStartDate)).getText().toString();
-                String endDate = ((TextView)findViewById(R.id.txtEndDate)).getText().toString();
+                Boolean done = false;
+                String ownerID = "";
+                ArrayList<SubTask> subTaskList = subTasks;
+                String id = "";
+                int startDate = Integer.parseInt( ((TextView)findViewById(R.id.txtStartDate)).getText().toString());
+                int endDate = Integer.parseInt( ((TextView)findViewById(R.id.txtEndDate)).getText().toString() );
                 String name = ((TextView)findViewById(R.id.txtTaskName)).getText().toString();
                 String description = ((TextView)findViewById(R.id.txtNotes)).getText().toString();
+                String status = "";
+
+                dB.addTask(name, startDate, description, endDate, ownerID, subTaskList, status);
 
             }
         });
         final Button addButton = findViewById(R.id.btnAdd);
-        button.setOnClickListener(new View.OnClickListener() {
+        addButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                String mToAdd = ((TextView)findViewById(R.id.txtSTName)).getText().toString();
-                if(!mToAdd.equals("")) {
-                    subTasks.add(new SubTask(mToAdd, false));
-                    names.add(mToAdd);
+                String subTaskToAdd = ((TextView)findViewById(R.id.txtSTName)).getText().toString();
+
+
+                if(!subTaskToAdd.equals("")) {
+                    subTasks.add(new SubTask(subTaskToAdd, false));
+                    names.add(subTaskToAdd);
+                    materialsTasksAdapter.notifyDataSetChanged();
 
                 }
             }
         });
+
+        final Button removeButton = findViewById(R.id.btnRemove);
+        removeButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String subTaskToRemove = ((TextView)findViewById(R.id.txtSTName)).getText().toString();
+
+                int indexOfSubTaskToBeRemoved = 9999999;
+
+                //Checks list if the input is valid
+                for(SubTask str: subTasks){
+
+                    if(str.getName().equals(subTaskToRemove)){
+                        indexOfSubTaskToBeRemoved = subTasks.indexOf(str);
+                        System.out.println("-------------------------------> IDENTICAL TEXT HAS BEEN FOUND");
+                        break;
+                    }
+                }
+
+                try {
+                    subTasks.remove(indexOfSubTaskToBeRemoved);
+                    materialsTasksAdapter.notifyDataSetChanged();
+                    System.out.println("-------------------------------> SUBTASK HAS BEEN REMOVED FROM LIST");
+                } catch (IndexOutOfBoundsException e) {
+                    CharSequence response = "Requested Material does not exist";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(getApplicationContext(), response, duration);
+                    toast.show();
+                    System.out.println("-------------------------------> TOAST HAS BEEN SHOWN");
+                }
+
+            }
+        });
+
+
     }
 
 }
---
+++ app/src/main/java/com/uottawa/choremanager/newTaskMaterialsAdapter.java
@@ -0,0 +1,46 @@
+package com.uottawa.choremanager;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Raymo on 2017-11-30.
+ */
+
+public class newTaskMaterialsAdapter extends ArrayAdapter {
+    private final Context context;
+    //private DataBase dB;
+    private final ArrayList<SubTask> subTaskList;
+
+    public newTaskMaterialsAdapter(Context context, ArrayList<SubTask> subTaskList){
+        super(context, R.layout.add_task, subTaskList);
+        this.context = context;
+        this.subTaskList = subTaskList;
+        //this.dB = MainActivity.getDB();
+
+        System.out.println("-----------------------------------REACHED CONSTRUCTOR");
+    }
+
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.subtask_template, parent, false);
+
+        TextView subTaskNameTextField = (TextView) rowView.findViewById(R.id.txtSubTask);
+        ImageView personImage = (ImageView) rowView.findViewById(R.id.imgPerson);
+
+        subTaskNameTextField.setText(subTaskList.get(position).getName());
+
+        System.out.println("-----------------------------------GET VIEW HAS BEEN CALLED");
+
+        return(rowView);
+
+    }
+}
